acc8c3a5e582798bff481aef45fd72baecb53b7d
NazarKr <nazarok1990@gmail.com>
2023-04-05 10:48:30 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://NazarKr:i04021990@cluster0.4hu99yv.mongodb.net/contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() =>
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000");
+  }))
+  .catch(error => console.log(error.message)
+);
+
+
